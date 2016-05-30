6cae1fc11de78442e751eedb1bc0018f4a334ac2
Nathan Gaberel <nathang@theodo.fr>
2016-05-30 18:43:27 +0100

    Add uuid to books.

--- src/core.js
+++ src/core.js
@@ -1,4 +1,5 @@
 import {List, fromJS} from 'immutable';
+import uuid from 'uuid';
 
 export const INITIAL_STATE = fromJS({
   library: {
@@ -11,6 +12,10 @@ export function setState(state, newState) {
 }
 
 export function addBook(state, book) {
+  if(!book.has('uuid')) {
+    book = book.set('uuid', uuid.v4());
+  }
+
   const books = state.getIn(['library', 'books'], List()).concat([book]);
 
   return state.merge({
--- test/core.js
+++ test/core.js
@@ -6,11 +6,11 @@ import {addBook} from '../src/core';
 describe('Core logic', () => {
   describe('addBook', () => {
     const newBook = fromJS({
-      "ISBN": "9780451417855",
-      "title": "Flatland",
-      "startDate": "2015-02",
-      "endDate": "2015-02",
-      "state": "read"
+      ISBN: '9780451417855',
+      title: 'Flatland',
+      startDate: '2015-02',
+      endDate: '2015-02',
+      state: 'read'
     });
 
     it('adds a book to the library', () => {
@@ -18,56 +18,56 @@ describe('Core logic', () => {
         library: {
           books: [
             {
-              "ISBN": "9782253001676",
-              "title": "Je suis Pilgrim",
-              "startDate": "2016",
-              "endDate": "2016-04-17",
-              "state": "read"
+              ISBN: '9782253001676',
+              title: 'Je suis Pilgrim',
+              startDate: '2016',
+              endDate: '2016-04-17',
+              state: 'read',
+              uuid: 'aaa'
             }
           ]
         }
       });
       const nextState = addBook(state, newBook);
 
-      expect(nextState).to.equal(fromJS({
-        library: {
-          books: [
-            {
-              "ISBN": "9782253001676",
-              "title": "Je suis Pilgrim",
-              "startDate": "2016",
-              "endDate": "2016-04-17",
-              "state": "read"
-            },
-            {
-              "ISBN": "9780451417855",
-              "title": "Flatland",
-              "startDate": "2015-02",
-              "endDate": "2015-02",
-              "state": "read"
-            }
-          ]
-        }
-      }))
+      const nextBooks = nextState.getIn(['library', 'books']);
+      expect(nextBooks.count()).to.equal(2);
+
+      const nextBook = nextBooks.get(1);
+      expect(nextBook).to.have.property('ISBN', '9780451417855');
+      expect(nextBook).to.have.property('title', 'Flatland');
+      expect(nextBook).to.have.property('startDate', '2015-02');
+      expect(nextBook).to.have.property('endDate', '2015-02');
+      expect(nextBook).to.have.property('state', 'read');
+      expect(nextBook).to.have.property('uuid');
     });
 
     it('creates a book list when empty', () => {
       const state = Map();
       const nextState = addBook(state, newBook);
 
-      expect(nextState).to.equal(fromJS({
+      expect(nextState).to.have.deep.property(['library', 'books']);
+
+      const nextBooks = nextState.getIn(['library', 'books']);
+      expect(nextBooks.count()).to.equal(1);
+    });
+
+    it('adds a uuid to the book', () => {
+      const state = fromJS({
         library: {
-          books: [
-            {
-              "ISBN": "9780451417855",
-              "title": "Flatland",
-              "startDate": "2015-02",
-              "endDate": "2015-02",
-              "state": "read"
-            }
-          ]
+          books: []
         }
-      }))
+      });
+      const nextState = addBook(state, newBook);
+
+      const books = nextState.getIn(['library', 'books']);
+      expect(books.count()).to.equal(1);
+
+      const book = books.get(0);
+      expect(book).to.have.property('uuid');
+
+      const uuid = book.get('uuid');
+      expect(uuid).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
     });
   });
 });
