a60752acb77a8e09c652ed7c26970fdaf4a3d520
Nathan Gaberel <nathang@theodo.fr>
2016-05-30 14:19:51 +0100

    Improve deploy script.

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 MAIN_BRANCH=master
 SITE_BRANCH=gh-pages
 
+# Build
 git checkout $MAIN_BRANCH
 npm run build
+cp -r dist dist-deploy
+
+# Commit
 git checkout $SITE_BRANCH
-cp dist/* .
+cp dist-deploy/* .
 git add index.html bundle.js
+rm -r dist-deploy
