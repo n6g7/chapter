443b2d2c1d8ce7ddba197a40f7d1662a9c626f6b
Nathan Gaberel <nathang@theodo.co.uk>
2016-06-12 16:42:27 +0100

    Improve hide-able bookshelves.

--- src/components/library/BookShelf.jsx
+++ src/components/library/BookShelf.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import capitalize from 'lodash/capitalize';
-import {List} from 'immutable';
+import { List } from 'immutable';
 import Button from '../common/Button';
 import BookList from './BookList';
 
@@ -14,18 +14,26 @@ export default React.createClass({
     type: React.PropTypes.string
   },
   render: function() {
-    if (this.props.hideWhenEmpty && this.props.books.isEmpty()) return false;
+    const { type } = this.props;
+    const sectionName = capitalize(type);
 
-    const sectionName = capitalize(this.props.type);
+    let classes = [type];
 
-    return <section className={this.props.type}>
+    let inner = <p className="announce">
+      Whoops, nothing here yet. Do you want to <Button label="Add a book" link="/new" /> ?
+    </p>;
+
+    if (this.props.hideWhenEmpty && this.props.books.isEmpty()) {
+      inner = null;
+      classes.push('hide');
+    }
+    else if (!this.props.books.isEmpty()) {
+      inner = <BookList books={this.props.books} />;
+    }
+
+    return <section className={classes.join(' ')}>
       <h2>{sectionName}</h2>
-      {this.props.books.isEmpty() ?
-        <p className="announce">
-          Whoops, nothing here yet. Do you want to <Button label="Add a book" link="/new" /> ?
-        </p> :
-        <BookList books={this.props.books} />
-      }
+      {inner}
     </section>;
   }
 });
