c0703a450424099c3d3afaa4866622027ebede6c
Nathan Gaberel <nathang@theodo.co.uk>
2016-08-06 15:23:24 +0100

    Improve build process.

--- build.sh
+++ build.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
+
+if [ "$#" -ne 1 ]
+then
+  echo "Need a version!"
+  exit
+fi
+
+PROJECT_NAME="Chapter"
 MAIN_BRANCH=master
 SITE_BRANCH=gh-pages
+VERSION=$1
 
 # Build
 git checkout $MAIN_BRANCH
 npm run build
 cp -r dist dist-deploy
+git stash
 
 # Commit
 git checkout $SITE_BRANCH
 cp dist-deploy/* .
 git add index.html bundle.js
 rm -r dist-deploy
+git commit -m "$PROJECT_NAME v$VERSION."
+
+# Come back
+git checkout $MAIN_BRANCH
+git stash pop
