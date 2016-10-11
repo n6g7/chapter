3cc089fd61a32a303f3480da517dce5450ae792f
Nathan Gaberel <nathang@theodo.co.uk>
2016-10-11 22:17:45 +0100

    Remove useless components.

--- src/components/ImportExport.jsx
+++ src/components/ImportExport.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { Map } from 'immutable';
 
-import Header from './common/Header';
 import Export from './io/Export';
 import Import from './io/Import';
 import './ImportExport.styl';
@@ -18,8 +17,6 @@ export default React.createClass({
     const { state, importState } = this.props;
 
     return <div>
-      <Header title="Export" backButton={true} />
-
       <section className="io">
         <Export state={state} />
         <Import importState={importState} />
