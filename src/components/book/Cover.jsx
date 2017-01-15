abe5bbc055540ad168824467517d420b108a4efd
Nathan Gaberel <nathang@theodo.co.uk>
2017-01-15 22:52:14 +0000

    Always show book's title on cover when image is absent.

--- src/components/book/Cover.jsx
+++ src/components/book/Cover.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {Map} from 'immutable';
-import Loader from '../common/Loader';
 
 import './Cover.styl';
 
 class Cover extends React.PureComponent {
   getImage() {
-    const { book, loading } = this.props;
+    const { book } = this.props;
     const url = book.getIn(['cover', 'image']);
 
-    return loading ? <Loader /> : <img src={url} alt={book.get('title')}/>;
+    return url
+      ? <img src={url} alt={book.get('title')}/>
+      : <img alt={book.get('title')}/>;
   }
 
   render() {
-    const { book, loading } = this.props;
-    const colour = !loading ? book.getIn(['cover', 'colour']) : null;
+    const { book } = this.props;
+    const colour = book.getIn(['cover', 'colour']);
 
     return <article className="cover" style={{ backgroundColor: colour}}>
       {this.getImage()}
@@ -23,8 +24,7 @@ class Cover extends React.PureComponent {
 }
 
 Cover.propTypes = {
-  book: React.PropTypes.instanceOf(Map).isRequired,
-  loading: React.PropTypes.bool
+  book: React.PropTypes.instanceOf(Map).isRequired
 };
 
 export default Cover;
