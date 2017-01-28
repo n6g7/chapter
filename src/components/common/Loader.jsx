75d747b55e78ec5b969a8f284d5fb03ca4694709
Nathan Gaberel <nathang@theodo.co.uk>
2017-01-28 19:29:34 +0000

    Improve Loader.

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -4,10 +4,25 @@ import './Loader.styl';
 
 class Loader extends React.PureComponent {
   render() {
-    return <div className="loader">
+    const { label, small, white } = this.props;
+
+    return <div className={`loader ${small ? 'small' : ''} ${white ? 'white' : ''}`}>
       <span></span>
+      { label }
     </div>
   }
 }
+
+Loader.propTypes = {
+  label: React.PropTypes.string.isRequired,
+  small: React.PropTypes.bool.isRequired,
+  white: React.PropTypes.bool.isRequired,
+};
+
+Loader.defaultProps = {
+  label: 'Loading',
+  small: false,
+  white: false,
+};
 
 export default Loader;
