5505507aca773534f42f02a98eec1c482c0d745c
Nathan Gaberel <nathang@theodo.co.uk>
2017-02-05 14:11:11 +0000

    Avoid white backgrounds.

--- src/services/apis/tint.js
+++ src/services/apis/tint.js
@@ -1,10 +1,21 @@
 import HttpClient from './client';
 
 const TINT_API = 'https://tint.gnab.fr/tint';
+const WHITE_THRESHOLD = 240;
+
+const averageGray = rgb => {
+  const channels = rgb.split(',').map(s => parseInt(s));
+  return channels.reduce((acc, x) => acc + x, 0) / channels.length;
+}
 
 function getMainColour(imageUrl) {
   return HttpClient.post(TINT_API, { url: imageUrl })
-    .then(data => `#${data.colours[0].hex}`);
+    .then(data => {
+      const colour = data.colours.find(colour => averageGray(colour.rgb) <= WHITE_THRESHOLD);
+      const hex = colour.hex || data.colours[0].hex
+
+      return `#${hex}`;
+    });
 }
 
 export default {
